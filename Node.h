8263829137cafdbd7d394acc992fe454bad2808f
gerardoalar <gerardo.alarcon2000@gmail.com>
2025-03-28 10:55:58 -0700

    Implemented Binary Tree and Traversal

--- Node.h
+++ Node.h
@@ -8,7 +8,15 @@ public:
     Node* left;
     Node* right;
 
-    Node(T value) : data(value), left(nullptr), right(nullptr){ } // TODO: Implement constructor here
+    Node(T value) : data(value), left(nullptr), right(nullptr) {
+    } // TODO: Implement constructor here
+    void setData(T value) { data = value; }
+    void setLeft(Node* Node) { left = Node; }
+    void setRight(Node* Node) { right = Node; }
+
+    Node* getLeft() { return left; }
+    Node* getRight() { return right; }
+    T getData() { return data; }
 };
 
 #endif // NODE_H
